ad2feff97257bbe66fa5319bd06cae0e062b176e
neeilgupta <gupt1141@purdue.edu>
2024-10-30 01:46:31 -0400

    Added a PostInterface for Post just incase we use it in the future. I kept all the methods the same in Post.

--- Main/src/Post.java
+++ Main/src/Post.java
@@ -1,7 +1,7 @@
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class Post {
+public class Post implements PostInterface {
     private int ID;
     private String content;
     private Main user;
@@ -12,42 +12,51 @@ public class Post {
     public Post(){
 
     }
+    @Override
     public int getID(){
         return ID;
     }
+    @Override
     public void setID(int ID){
         this.ID = ID;
     }
+    @Override
     public String getContent(){
         return content;
     }
+    @Override
     public void setContent(String content){
         this.content = content;
     }
+    @Override
     public Main getUser(){
         return user;
     }
+    @Override
     public void setUser(Main user){
         this.user = user;
     }
-
+    @Override
     public LocalDateTime getDateTime(){
         return dateTime;
     }
-
+    @Override
     public void setDateTime(LocalDateTime dateTime){
         this.dateTime = dateTime;
     }
-
+    @Override
     public ArrayList<Comment> getComments() {
         return comments;
     }
+    @Override
     public void setComments(ArrayList<Comment> comments){
         this.comments = comments;
     }
+    @Override
     public ArrayList<Main> getLikes(){
         return likes;
     }
+    @Override
     public void setLikes(ArrayList<Main> likes){
         this.likes = likes;
     }
---
+++ Main/src/PostInterface.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public interface PostInterface {
+    int getID();
+    void setID(int ID);
+    String getContent();
+    void setContent(String content);
+    Main getUser();
+    void setUser(Main user);
+    LocalDateTime getDateTime();
+    void setDateTime(LocalDateTime dateTime);
+    ArrayList<Comment> getComments();
+    void setComments(ArrayList<Comment> comments);
+    ArrayList<Main> getLikes();
+    void setLikes(ArrayList<Main> likes);
+}
