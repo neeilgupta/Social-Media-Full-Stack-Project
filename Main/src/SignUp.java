336e9df8c7a6a6b022b5706fed6bbc9b6f8dbe97
hhatamiy <hhatamiy@gmail.com>
2024-10-30 17:16:20 -0400

    Password updates

--- Main/src/SignUp.java
+++ Main/src/SignUp.java
@@ -10,8 +10,10 @@ public class SignUp implements Runnable {
     private String username;
     private String displayName;
     private String password;
+    private boolean validEmailEndings;
     public boolean usernameTaken; // Doesn't save usernames as of yet
     private static Set<String> usernames = new HashSet<>();
+    private int result;
 
 
     @Override
@@ -21,20 +23,32 @@ public class SignUp implements Runnable {
         frame.add(panel);
         frame.setResizable(true);
 
-        panel.add(new JLabel("Please enter your email address:"));
         while (true) {
-            email = JOptionPane.showInputDialog(panel, "Please enter your email address:");
-            if (!email.chars().allMatch(c -> (c >= 48 && c <= 57) || (c >= 64 && c <= 90) || (c >= 45 && c <= 46) ||
-                    (c >= 97 && c <= 122))) {
-                JOptionPane.showMessageDialog(panel, "Not a valid email address");
-            } else if (!email.contains("@") || !email.contains(".")) {
-                JOptionPane.showMessageDialog(panel, "Not a valid email address");
-            } else {
-                break;
+//            result = JOptionPane.showConfirmDialog(null, panel, "Please enter a valid email address",
+//                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+//            email = JOptionPane.showInputDialog(panel, "Please enter your email address:");
+            if (result == JOptionPane.OK_OPTION) {
+                email = JOptionPane.showInputDialog(frame, "Enter your email address");
+                if (email == null) {
+                    throw new NullPointerException();
+                }
+                if (email.length() < 6) {
+                    JOptionPane.showMessageDialog(panel, "Not a valid email address");
+                }
+                if (!email.chars().allMatch(c -> (c >= 48 && c <= 57) || (c >= 64 && c <= 90) || (c >= 45 && c <= 46) ||
+                        (c >= 97 && c <= 122))) {
+                    JOptionPane.showMessageDialog(panel, "Not a valid email address");
+                } else if (!email.contains("@") || !email.contains(".")) {
+                    JOptionPane.showMessageDialog(panel, "Not a valid email address");
+                } else {
+                    break;
+                }
+            } else if (result == JOptionPane.CANCEL_OPTION) {
+                System.exit(0);
             }
         }
         panel.removeAll();
-
+        frame.remove(panel);
 //        String verificationCode = String.valueOf(new Random().nextInt(10000)); for later, for fun ;)
 
         panel.add(new JLabel("Please create a new password:"));
@@ -46,15 +60,23 @@ public class SignUp implements Runnable {
         JPasswordField passwordField = new JPasswordField(32);
         passwordField.setEchoChar('*');
         panel.add(new JScrollPane(passwordField));
+        String[] generatedPassword = {""};
+        button.addActionListener(e -> {
+            String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()-_=+/?.>,<`~[{]}:;";
+            for (int i = 0; i < 30 ; i++) {
+                int random = new Random().nextInt(chars.length());
+                generatedPassword[i] += chars.charAt(random);
+                passwordField.setText(generatedPassword[i]);
+            }
+            //work in progress
+        });
 
 
-        int result;
         while (true) {
             result = JOptionPane.showConfirmDialog(null, panel, "Create Password",
                     JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
             if (result == JOptionPane.OK_OPTION) {
-                char[] passwordChars = passwordField.getPassword();
-                password = new String(passwordChars);
+                password = new String(passwordField.getPassword());
                 if (password.length() < 8) {
                     JOptionPane.showMessageDialog(panel, "Password must be at least 8 characters");
                 } else if (password.length() > 32) {
@@ -62,10 +84,16 @@ public class SignUp implements Runnable {
                 } else {
                     break;
                 }
-            } else if (result == JOptionPane.CANCEL_OPTION) {
+            } else {
                 frame.dispose();
-            };
+                System.exit(0);
+                break;
+            }
         }
+        JOptionPane.showMessageDialog(panel, "Password set successfully " + password);
+        panel.removeAll();
+        frame.remove(panel);
+        frame.dispose();
 
         panel.add(new JLabel("Please enter your username:"));
         while (true) {
@@ -126,6 +154,5 @@ public class SignUp implements Runnable {
     public void setPassword(String password) {
         this.password = password;
     }
-
 
 }
