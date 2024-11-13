df3f38cc5ca14c6b9d0e727ca2c19b1fbd7f831f
hhatamiy <hhatamiy@gmail.com>
2024-11-13 00:06:35 -0500

    Uopdated client
    (cherry picked from commit 38e72e28c6b4b262bd76fdb051edbdb03a53e087)

--- Main/src/Server.java
+++ Main/src/Server.java
@@ -23,6 +23,8 @@ public class Server {
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
+
+
             //process client requests
             //use existing login and sign up method processing
 
